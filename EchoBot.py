c5c836de526895e93a9a79ab2e863e4a67b618d2
KilluaZoludyck <144722702+KilluaZoludyck@users.noreply.github.com>
2025-02-04 10:24:15 +0300

    EchoBot

---
+++ EchoBot.py
@@ -0,0 +1,25 @@
+# Импорт библиотек
+import telebot
+from telebot import types
+
+# Берём у BotFather токен (бот должен быть заранее создан)
+token = '8080066714:AAH8JI7BrLukLmOsluQ1yQY3uEDPT9qph8U'
+# Определяем нашего бота с помощью токена
+bot = telebot.TeleBot(token)
+
+
+# Определяем команду боту для /start
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,
+                     "Йоу, рад видеть! Это Эхо-бот, можешь написать сообщение и я его повторю!")
+    # Отправляем сообщение по id чата
+
+
+# Функция для отправки сообщения пользователю
+@bot.message_handler(content_types='text')  # Для текстовых сообщений
+def message_reply(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.infinity_polling()
